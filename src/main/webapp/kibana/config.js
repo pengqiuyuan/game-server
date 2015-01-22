c66a06d75bc1e3ee4a4dc657dec1342da76336ff
pengqiuyuan <pengqiuyuanfj@gmail.com>
2015-01-22 20:17:13 +0800

    1、kibana

--- src/main/webapp/kibana/config.js
+++ src/main/webapp/kibana/config.js
@@ -24,7 +24,7 @@ function (Settings) {
      * kibana installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: "http://"+window.location.hostname+":9200",
+    elasticsearch: "http://10.0.10.111:9200",
     
     /** @scratch /configuration/config.js/5
      *
