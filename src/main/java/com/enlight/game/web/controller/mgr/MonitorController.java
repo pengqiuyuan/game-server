4dee6db59458cce2b54461dd2329582f9c20be0d
pengqiuyuan <pengqiuyuanfj@gmail.com>
2015-11-25 18:41:12 +0800

    监控

--- src/main/java/com/enlight/game/web/controller/mgr/MonitorController.java
+++ src/main/java/com/enlight/game/web/controller/mgr/MonitorController.java
@@ -1,8 +1,6 @@
 package com.enlight.game.web.controller.mgr;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,13 +11,10 @@ import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,69 +23,48 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springside.modules.web.Servlets;
 
-import com.enlight.game.entity.EnumCategory;
+import com.enlight.game.entity.Monitor;
 import com.enlight.game.entity.Stores;
 import com.enlight.game.entity.User;
 import com.enlight.game.service.account.AccountService;
 import com.enlight.game.service.account.ShiroDbRealm.ShiroUser;
-import com.enlight.game.service.enumCategory.EnumCategoryService;
-import com.enlight.game.service.enumFunction.EnumFunctionService;
-import com.enlight.game.service.serverZone.ServerZoneService;
+import com.enlight.game.service.monitor.MonitorService;
 import com.enlight.game.service.store.StoreService;
-import com.enlight.game.entity.EnumFunction;
 import com.google.common.collect.Maps;
 
-@Controller("enumFunctionController")
-@RequestMapping("/manage/enumFunction")
-public class EnumFunctionController extends BaseController{
+@Controller("monitorController")
+@RequestMapping("/manage/monitor")
+public class MonitorController extends BaseController{
 
-	private static final String PAGE_SIZE = "15";
-
-	private static final Logger logger = LoggerFactory.getLogger(EnumFunctionController.class);
-	private static Map<String, String> sortTypes = Maps.newLinkedHashMap();
+	private static final String PAGE_SIZE = "50";
 	
-	static {
-		sortTypes.put("auto", "自动");
+	private static final Logger logger = LoggerFactory.getLogger(MonitorController.class);
+	
+	private static Map<String,String> sortTypes = Maps.newLinkedHashMap();
+	
+	static{
+		sortTypes.put("auto","自动");
+		sortTypes.put("key", "key");
 	}
-
+	
 	public static Map<String, String> getSortTypes() {
 		return sortTypes;
 	}
-
+	
 	public static void setSortTypes(Map<String, String> sortTypes) {
-		EnumFunctionController.sortTypes = sortTypes;
+		MonitorController.sortTypes = sortTypes;
 	}
-	
-	@Override
-	@InitBinder
-	protected void initBinder(ServletRequestDataBinder binder){
-		binder.registerCustomEditor(Date.class,"registerDate",new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
-	}
-	
-	@Autowired
-	private AccountService accountService;
-	
-	@Autowired
-	private EnumFunctionService enumFunctionService;
 	
 	@Autowired
 	private StoreService storeService;
 	
 	@Autowired
-	private ServerZoneService serverZoneService;
+	private AccountService accountService;
 	
 	@Autowired
-	private EnumCategoryService enumCategoryService;
+	private MonitorService monitorService;
 	
-	/**
-	 *  用户管理首页
-	 * @param pageNumber 当前	 
-	 * @param pageSize   显示条数
-	 * @param sortType  排序
-	 * @param model   返回对象
-	 * @param request  封装的请	
-	 */
-	@RequestMapping(value = "index", method = RequestMethod.GET)
+	@RequestMapping(value = "index",method=RequestMethod.GET)
 	public String index(@RequestParam(value = "page", defaultValue = "1") int pageNumber,
 			@RequestParam(value = "page.size", defaultValue = PAGE_SIZE) int pageSize,
 			@RequestParam(value = "sortType", defaultValue = "auto")String sortType, Model model,
@@ -98,8 +72,13 @@ public class EnumFunctionController extends BaseController{
 		Long userId = getCurrentUserId();
 		ShiroUser user = getCurrentUser();
 		User u = accountService.getUser(user.id);
+		logger.info("userId"+userId+"监控");
 		Map<String, Object> searchParams = Servlets.getParametersStartingWith(request, "search_");
-		Page<EnumFunction> enumFunctions = enumFunctionService.findEnumFunctionsByCondition(userId,searchParams, pageNumber, pageSize, sortType);
+		Page<Monitor> monitors = monitorService.findMonitorByCondition(userId,searchParams, pageNumber, pageSize, sortType);
+		model.addAttribute("monitors", monitors);
+		model.addAttribute("sortType", sortType);
+		model.addAttribute("sortTypes", sortTypes);
+		// 将搜索条件编码成字符串，用于排序，分页的URL
 		
 		if (!u.getRoles().equals(User.USER_ROLE_ADMIN)) {
 			List<Stores> stores = new ArrayList<Stores>();
@@ -110,14 +89,9 @@ public class EnumFunctionController extends BaseController{
 			List<Stores> stores =  storeService.findList();
 			model.addAttribute("stores", stores);
 		}
-		List<EnumCategory> enumCategories = enumCategoryService.findAll();
-		model.addAttribute("enumCategories", enumCategories);
-		model.addAttribute("enumFunctions", enumFunctions);
-		model.addAttribute("sortType", sortType);
-		model.addAttribute("sortTypes", sortTypes);
-		// 将搜索条件编码成字符串，用于排序，分页的URL
+
 		model.addAttribute("searchParams", Servlets.encodeParameterStringWithPrefix(searchParams, "search_"));
-		return "/enumFunction/index";
+		return "/monitor/index";
 	}
 	
 	/**
@@ -136,25 +110,22 @@ public class EnumFunctionController extends BaseController{
 			List<Stores> stores =  storeService.findList();
 			model.addAttribute("stores", stores);
 		}	
-		List<EnumCategory> enumCategories = enumCategoryService.findAll();
-		model.addAttribute("enumCategories", enumCategories);
-		return "/enumFunction/add";
+		return "/monitor/add";
 	}
 	
 	/**
 	 * 新增
 	 */
 	@RequestMapping(value = "/save",method=RequestMethod.POST)
-	public String save(EnumFunction enumFunction,ServletRequest request,RedirectAttributes redirectAttributes,Model model){
-		enumFunctionService.save(enumFunction);
+	public String save(Monitor monitor,ServletRequest request,RedirectAttributes redirectAttributes,Model model){
+		monitorService.save(monitor);
 		redirectAttributes.addFlashAttribute("message", "新增成功");
-		return "redirect:/manage/enumFunction/index?search_LIKE_gameId="+enumFunction.getGameId()+"&search_LIKE_categoryId="+enumFunction.getCategoryId();
+		return "redirect:/manage/monitor/index?search_LIKE_storeId="+monitor.getStoreId();
 	}
-	
 	
 	@RequestMapping(value="edit",method=RequestMethod.GET)
 	public String edit(@RequestParam(value="id")long id,Model model){
-		EnumFunction enumFunction = enumFunctionService.findById(id);
+		Monitor monitor = monitorService.findByMoId(id);
 		ShiroUser user = getCurrentUser();
 		User u = accountService.getUser(user.id);
 		if (!u.getRoles().equals(User.USER_ROLE_ADMIN)) {
@@ -166,20 +137,18 @@ public class EnumFunctionController extends BaseController{
 			List<Stores> stores =  storeService.findList();
 			model.addAttribute("stores", stores);
 		}	
-		List<EnumCategory> enumCategories = enumCategoryService.findAll();
-		model.addAttribute("enumCategories", enumCategories);
-		model.addAttribute("enumFunction", enumFunction);
-		return "/enumFunction/edit";
+		model.addAttribute("monitor", monitor);
+		return "/monitor/edit";
 	}
 	
 	/**
 	 * 修改
 	 */
 	@RequestMapping(value = "/update",method=RequestMethod.POST)
-	public String update(EnumFunction enumFunction,ServletRequest request,RedirectAttributes redirectAttributes){
-		enumFunctionService.update(enumFunction);
+	public String update(Monitor monitor,ServletRequest request,RedirectAttributes redirectAttributes){
+		monitorService.update(monitor);
 		redirectAttributes.addFlashAttribute("message", "更新成功");
-		return "redirect:/manage/enumFunction/index?search_LIKE_gameId="+enumFunction.getGameId()+"&search_LIKE_categoryId="+enumFunction.getCategoryId();
+		return "redirect:/manage/monitor/index?search_LIKE_storeId="+monitor.getStoreId();
 	}
 	
 	/**
@@ -190,21 +159,11 @@ public class EnumFunctionController extends BaseController{
 	@RequestMapping(value = "del", method = RequestMethod.DELETE)
 	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
-	public Map<String,Object> delServerZone(@RequestParam(value = "id")Long id) throws Exception{
+	public Map<String,Object> delMonitorZone(@RequestParam(value = "id")Long id) throws Exception{
 		 Map<String,Object> map = new HashMap<String, Object>();
-		 enumFunctionService.delById(id);
+		 monitorService.delById(id);
 		 map.put("success", "true");
 		 return map;
-	}
-	
-	@RequestMapping(value = "/checkEnumRole")
-	@ResponseBody
-	public String checkServerId(@RequestParam("enumRole") String enumRole) {
-		if (enumFunctionService.findByEnumRole(enumRole) == null) {
-			return "true";
-		} else {
-			return "false";
-		}
 	}
 	
 	/**
