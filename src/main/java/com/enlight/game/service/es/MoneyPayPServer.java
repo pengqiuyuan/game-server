e4cce478dbe6e35e8a850008cce6ef92ee4798ef
pengqiuyuan <pengqiuyuanfj@gmail.com>
2015-12-16 17:54:34 +0800

    fix

--- src/main/java/com/enlight/game/service/es/MoneyPayPServer.java
+++ src/main/java/com/enlight/game/service/es/MoneyPayPServer.java
@@ -209,7 +209,7 @@ public class MoneyPayPServer {
 				        .setTypes(type)
 				        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
 				        .setQuery(builder)
-				        .addSort("date", SortOrder.ASC)
+				        .addSort("date", SortOrder.DESC)
 				        .setFrom(0).setSize(daysBetween(dateFrom,dateTo)).setExplain(true)
 				        .execute()
 				        .actionGet();
@@ -233,7 +233,7 @@ public class MoneyPayPServer {
 				        .setTypes(type)
 				        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
 				        .setQuery(builder)
-				        .addSort("date", SortOrder.ASC)
+				        .addSort("date", SortOrder.DESC)
 				        .setFrom(0).setSize(daysBetween(dateFrom,dateTo)).setExplain(true)
 				        .execute()
 				        .actionGet();
