769dba32313715133ed1f4fb745e118b106558e3
pengqiuyuan <pengqiuyuanfj@gmail.com>
2016-06-30 19:40:20 +0800

    灰度帐号
    灰度帐号

---
+++ src/main/java/com/enlight/game/entity/gm/fb/ServerStatusAccountAddOrUpdate.java
@@ -0,0 +1,61 @@
+package com.enlight.game.entity.gm.fb;
+
+import java.util.List;
+
+/**
+ * @author dell
+ * 灰度账号
+ */
+public class ServerStatusAccountAddOrUpdate {
+	
+	private String serverZoneId;
+	
+	private String gameId;
+	
+	private String serverId;
+
+	private String platForm;
+	
+	private String account;
+
+	public String getPlatForm() {
+		return platForm;
+	}
+
+	public void setPlatForm(String platForm) {
+		this.platForm = platForm;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	
+
+	public String getServerZoneId() {
+		return serverZoneId;
+	}
+
+	public void setServerZoneId(String serverZoneId) {
+		this.serverZoneId = serverZoneId;
+	}
+
+	public String getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
+
+	public String getServerId() {
+		return serverId;
+	}
+
+	public void setServerId(String serverId) {
+		this.serverId = serverId;
+	}
+}
---
+++ src/main/java/com/enlight/game/entity/gm/xyj/ServerStatusGrayList.java
@@ -0,0 +1,31 @@
+package com.enlight.game.entity.gm.xyj;
+
+/**
+ * @author dell
+ * 灰度账号
+ */
+public class ServerStatusGrayList {
+	
+	private String platForm;
+	
+	private String account;
+
+	public String getPlatForm() {
+		return platForm;
+	}
+
+	public void setPlatForm(String platForm) {
+		this.platForm = platForm;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	
+	
+
+}
