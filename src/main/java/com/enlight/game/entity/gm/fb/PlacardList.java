05a860466ae3e74ca9edc836008f23f1cc75ab93
pengqiuyuan <pengqiuyuanfj@gmail.com>
2016-07-04 19:48:17 +0800

    PlacardList
    PlacardList

---
+++ src/main/java/com/enlight/game/entity/gm/fb/PlacardList.java
@@ -0,0 +1,21 @@
+package com.enlight.game.entity.gm.fb;
+
+import java.util.List;
+
+/**
+ * @author dell
+ *
+ */
+public class PlacardList {
+	
+	private List<Placard> placardList;
+
+	public List<Placard> getPlacardList() {
+		return placardList;
+	}
+
+	public void setPlacardList(List<Placard> placardList) {
+		this.placardList = placardList;
+	}
+
+}
---
+++ src/main/java/com/enlight/game/entity/gm/xyj/PlacardList.java
@@ -0,0 +1,21 @@
+package com.enlight.game.entity.gm.xyj;
+
+import java.util.List;
+
+/**
+ * @author dell
+ *
+ */
+public class PlacardList {
+	
+	private List<Placard> placardList;
+
+	public List<Placard> getPlacardList() {
+		return placardList;
+	}
+
+	public void setPlacardList(List<Placard> placardList) {
+		this.placardList = placardList;
+	}
+
+}
