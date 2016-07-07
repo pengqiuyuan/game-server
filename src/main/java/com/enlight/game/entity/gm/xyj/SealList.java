8eaff23304258c21de5174e689312a6c1e3367f3
pengqiuyuan <pengqiuyuanfj@gmail.com>
2016-07-07 19:25:24 +0800

    seal
    seal

---
+++ src/main/java/com/enlight/game/entity/gm/xyj/SealList.java
@@ -0,0 +1,79 @@
+package com.enlight.game.entity.gm.xyj;
+
+/**
+ * @author dell
+ * 禁言
+ */
+public class SealList {
+	
+	private String platForm;
+	
+	private String guid;
+	
+	private String name;
+	
+	private String account;
+	
+	private String sealTime;
+	
+	private String sealStart;
+	
+	private String sealEnd;
+
+	public String getPlatForm() {
+		return platForm;
+	}
+
+	public void setPlatForm(String platForm) {
+		this.platForm = platForm;
+	}
+
+	public String getGuid() {
+		return guid;
+	}
+
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getSealTime() {
+		return sealTime;
+	}
+
+	public void setSealTime(String sealTime) {
+		this.sealTime = sealTime;
+	}
+
+	public String getSealStart() {
+		return sealStart;
+	}
+
+	public void setSealStart(String sealStart) {
+		this.sealStart = sealStart;
+	}
+
+	public String getSealEnd() {
+		return sealEnd;
+	}
+
+	public void setSealEnd(String sealEnd) {
+		this.sealEnd = sealEnd;
+	}
+
+}
