1a8d2651f5813a83932fe19b8fe2caea22a8bae3
pengqiuyuan <pengqiuyuanfj@gmail.com>
2016-07-07 16:46:15 +0800

    GagList
    GagList

---
+++ src/main/java/com/enlight/game/entity/gm/kds/GagList.java
@@ -0,0 +1,79 @@
+package com.enlight.game.entity.gm.kds;
+
+/**
+ * @author dell
+ * 禁言
+ */
+public class GagList {
+	
+	private String platForm;
+	
+	private String guid;
+	
+	private String name;
+	
+	private String account;
+	
+	private String gagTime;
+	
+	private String gagStart;
+	
+	private String gagEnd;
+
+	public String getPlatForm() {
+		return platForm;
+	}
+
+	public void setPlatForm(String platForm) {
+		this.platForm = platForm;
+	}
+
+	public String getGuid() {
+		return guid;
+	}
+
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getGagStart() {
+		return gagStart;
+	}
+
+	public void setGagStart(String gagStart) {
+		this.gagStart = gagStart;
+	}
+
+	public String getGagEnd() {
+		return gagEnd;
+	}
+
+	public void setGagEnd(String gagEnd) {
+		this.gagEnd = gagEnd;
+	}
+	
+	public String getGagTime() {
+		return gagTime;
+	}
+
+	public void setGagTime(String gagTime) {
+		this.gagTime = gagTime;
+	}
+
+}
